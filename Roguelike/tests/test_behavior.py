cf14f66a0ffed87eb77a1f63dbbae5843dff7940
Тарасов Денис <dt6cgsg@gmail.com>
2022-05-02 21:26:04 +0300

    Fix tests

--- Roguelike/tests/test_behavior.py
+++ Roguelike/tests/test_behavior.py
@@ -3,6 +3,7 @@ import unittest
 from pathlib import Path
 
 from dune_rogue.logic.actions import Action
+from dune_rogue.logic.ai.behavior import build_priority
 from dune_rogue.logic.entities.player_character import PlayerCharacter
 from dune_rogue.logic.levels.level import Level
 from dune_rogue.logic.levels.mediator import LevelMediator
@@ -21,9 +22,9 @@ class BehaviorTest(unittest.TestCase):
         aggressive = self.corridor.acting_entities[2]
         passive = self.corridor.acting_entities[3]
 
-        priority = aggressive.behavior.build_priority(self.corridor.w, self.corridor.h,
-                                                      self.player.x, self.player.y,
-                                                      aggressive.x, aggressive.y, mediator)
+        priority = build_priority(self.corridor.w, self.corridor.h,
+                                  self.player.x, self.player.y,
+                                  aggressive.x, aggressive.y, mediator)
 
         target = [[-2.0] * self.corridor.w,
                   [-2.0] + [round(0.99 ** i, 2) for i in range(self.corridor.w - 2)] + [-2.0],
@@ -31,9 +32,9 @@ class BehaviorTest(unittest.TestCase):
         priority = list(map(lambda x: list(map(lambda e: round(e, 2), x)), priority))
         self.assertListEqual(priority, target)
 
-        priority = coward.behavior.build_priority(self.corridor.w, self.corridor.h,
-                                                  self.player.x, self.player.y,
-                                                  coward.x, coward.y, mediator)
+        priority = build_priority(self.corridor.w, self.corridor.h,
+                                  self.player.x, self.player.y,
+                                  coward.x, coward.y, mediator)
         target = [[-2.0] * self.corridor.w,
                   [-2.0] + [round(0.99 ** i, 2) for i in range(2)] + [-2.0] * (self.corridor.w - 3),
                   [-2.0] * self.corridor.w]
@@ -49,9 +50,9 @@ class BehaviorTest(unittest.TestCase):
         self.assertEqual((aggressive.x, aggressive.y), (aggressive_prev[0] - 1, aggressive_prev[1]))
         self.assertEqual((passive.x, passive.y), passive_prev)
 
-        priority = aggressive.behavior.build_priority(self.corridor.w, self.corridor.h,
-                                                      self.player.x, self.player.y,
-                                                      aggressive.x, aggressive.y, mediator)
+        priority = build_priority(self.corridor.w, self.corridor.h,
+                                  self.player.x, self.player.y,
+                                  aggressive.x, aggressive.y, mediator)
 
         target = [[-2.0] * self.corridor.w,
                   [-2.0] + [round(0.99 ** i, 2) for i in range(self.corridor.w - 3)] + [-2.0, -2.0],
@@ -59,9 +60,9 @@ class BehaviorTest(unittest.TestCase):
         priority = list(map(lambda x: list(map(lambda e: round(e, 2), x)), priority))
         self.assertListEqual(priority, target)
 
-        priority = coward.behavior.build_priority(self.corridor.w, self.corridor.h,
-                                                  self.player.x, self.player.y,
-                                                  coward.x, coward.y, mediator)
+        priority = build_priority(self.corridor.w, self.corridor.h,
+                                  self.player.x, self.player.y,
+                                  coward.x, coward.y, mediator)
         target = [[-2.0] * self.corridor.w,
                   [-2.0] + [round(0.99 ** i, 2) for i in range(3)] + [-2.0] * (self.corridor.w - 4),
                   [-2.0] * self.corridor.w]
